ebe9664f3ad4bfe0ee82dc0d303df2cfd09d3a5b
A.R <richardadrien0@gmail.com>
2019-03-20 09:50:50 +0100

    correction ??

---
+++ Game.Penguins/Game.Penguins.Core/Code/Player/Player.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Game.Penguins.Core.Interfaces.Game.Players;
+
+namespace Game.Penguins.Core.Code.Player
+{
+    /*
+    public class Player : IPlayer
+    {
+        
+        
+        public void CreatePlayer()
+        {
+
+        }
+    }
+*/
+
+}
