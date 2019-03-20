9034d4237503db03de971d0b7aa8c87f42ee218d
Nicolas Asri <nicolas.asri@gmail.com>
2019-03-20 11:57:13 +0100

    add class Plateau

---
+++ Game.Penguins/Game.Penguins.Core/Code/GameBoard/Plateau.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Game.Penguins.Core.Interfaces.Game.GameBoard;
+
+
+namespace Game.Penguins.Core.Code.GameBoard
+{
+    class Plateau : IBoard
+    {
+
+        private ICell[,] m_board;
+
+        // On implémente la propriété Board accessible en lecture.
+        public ICell[,] Board
+        {
+            get { return m_board; }
+        }
+
+        public void CreationPlateau()
+        {
+            m_board = new ICell[8,8];
+
+
+        }
+
+    }
+    
+}
