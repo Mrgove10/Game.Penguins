a1808c85e49930435e5540995641c5b74e060f67
A.R <richardadrien0@gmail.com>
2019-03-20 11:50:31 +0100

    [mod] unit test

--- Game.Penguins/Game.Penguins.Core/Code/Player/Player.cs
+++ Game.Penguins/Game.Penguins.Core/Code/Player/Player.cs
@@ -1,7 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using Game.Penguins.Core.Interfaces.Game.Players;
+﻿using Game.Penguins.Core.Interfaces.Game.Players;
+using System;
 
 namespace Game.Penguins.Core.Code.Player
 {
@@ -13,18 +11,31 @@ namespace Game.Penguins.Core.Code.Player
         /// <param name="name"></param>
         /// <param name="playerType"></param>
         /// <param name="playerColor"></param>
-        public Player(string name, PlayerType playerType, PlayerColor playerColor)
+        /// <param name="numberOfPenguins"></param>
+        public Player(string name, PlayerType playerType, PlayerColor playerColor, int numberOfPenguins)
         {
             Identifier = new Guid();
             Name = name;
             Color = PlayerColor.Blue;
+            Points = 0;
+
+            if (numberOfPenguins < 0 || numberOfPenguins > 4)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+            else
+            {
+                Penguins = numberOfPenguins;
+            }
         }
+
         public Guid Identifier { get; }
         public PlayerType PlayerType { get; }
         public PlayerColor Color { get; }
         public string Name { get; }
         public int Points { get; }
         public int Penguins { get; }
+
         public event EventHandler StateChanged;
     }
 }
