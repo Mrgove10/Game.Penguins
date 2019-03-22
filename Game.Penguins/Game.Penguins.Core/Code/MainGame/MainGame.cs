5b34d78ef19b8fdeba60b2f9681a08430bee434e
A.R <richardadrien0@gmail.com>
2019-03-22 16:11:26 +0100

    [mod]implemented interfaces

--- Game.Penguins/Game.Penguins.Core/Code/MainGame/MainGame.cs
+++ Game.Penguins/Game.Penguins.Core/Code/MainGame/MainGame.cs
@@ -1,35 +1,42 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using Game.Penguins.Core.Code.GameBoard;
 using Game.Penguins.Core.Interfaces.Game.GameBoard;
 using Game.Penguins.Core.Interfaces.Game.Players;
+using System;
+using System.Collections.Generic;
 
 namespace Game.Penguins.Core.Code.MainGame
 {
     public class MainGame : IGame
     {
-        MainGame()
+        private MainGame()
         {
-                
+            Board = new Plateau(8, 8);
+            NextAction = null;
+            CurrentPlayer = null;
+            Players = new List<IPlayer>();
+            AddPlayer();
         }
 
         public IBoard Board { get; }
         public NextActionType NextAction { get; }
         public IPlayer CurrentPlayer { get; }
         public IList<IPlayer> Players { get; }
+
         public event EventHandler StateChanged;
+
         public void AddPlayer(string playerName, PlayerType playerType)
         {
+            Players.Add(new Player(playerName, playerType));
             //ajouter les differents joueur
             throw new NotImplementedException();
         }
-
+            
         public void StartGame()
         {
             //Generation de la grille
             throw new NotImplementedException();
         }
-        
+
         /// <summary>
         /// Places the penguins on the board whit an X and Y parametre
         /// </summary>
@@ -37,10 +44,10 @@ namespace Game.Penguins.Core.Code.MainGame
         /// <param name="y"></param>
         public void PlacePenguinManual(int x, int y)
         {
-
             throw new NotImplementedException();
         }
 
+        //
         public void PlacePenguin()
         {
             throw new NotImplementedException();
