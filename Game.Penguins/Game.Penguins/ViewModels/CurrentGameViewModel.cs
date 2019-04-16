7b219a1061a680728a36bccaf679707dcdcd37b0
A.R <richardadrien0@gmail.com>
2019-04-16 15:51:12 +0200

    added penguins color

--- Game.Penguins/Game.Penguins/ViewModels/CurrentGameViewModel.cs
+++ Game.Penguins/Game.Penguins/ViewModels/CurrentGameViewModel.cs
@@ -32,6 +32,18 @@ namespace Game.Penguins.ViewModels
 
         public bool HasNextView => false;
 
+        #region Colors
+
+        public bool IsBlue { get; private set; }
+
+        public bool IsYellow { get; private set; }
+
+        public bool IsGreen { get; private set; }
+
+        public bool IsRed { get; private set; }
+
+        #endregion
+
         #region Interactions
 
         private bool selectFirst = true;
@@ -302,6 +314,17 @@ namespace Game.Penguins.ViewModels
                                     game.CurrentPlayer.PlayerType != PlayerType.Human;
             IsMoveMyPenguinAction = game.NextAction == NextActionType.MovePenguin &&
                                     game.CurrentPlayer.PlayerType == PlayerType.Human;
+
+
+            IsBlue = game.CurrentPlayer != null && game.CurrentPlayer.Color == PlayerColor.Blue;
+            IsYellow = game.CurrentPlayer != null && game.CurrentPlayer.Color == PlayerColor.Yellow;
+            IsGreen = game.CurrentPlayer != null && game.CurrentPlayer.Color == PlayerColor.Green;
+            IsRed = game.CurrentPlayer != null && game.CurrentPlayer.Color == PlayerColor.Red;
+
+            RaisePropertyChanged(nameof(IsBlue));
+            RaisePropertyChanged(nameof(IsYellow));
+            RaisePropertyChanged(nameof(IsGreen));
+            RaisePropertyChanged(nameof(IsRed));
         }
 
         public IApplicationContentView GetPreviousView()
